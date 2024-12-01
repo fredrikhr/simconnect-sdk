ce9e34a4e11953f0e99e2a1c7a70c1f51cb4dfcb
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-12-01 15:18:08 +0100

    Update SimConnect ClangSharp MSBuild targets

--- generator/FredrikHr.MicrosoftFlightSimulator.SimConnectSdk.ClangSharp/SimConnect.prepend.h
+++ generator/FredrikHr.MicrosoftFlightSimulator.SimConnectSdk.ClangSharp/SimConnect.prepend.h
@@ -3,13 +3,15 @@
 typedef unsigned char SIMCONNECT_STRINGCHAR;
 #define SIMCONNECT_STRING(name, size) SIMCONNECT_STRINGCHAR name[size]
 
-#ifdef MSVC
-#define SIMCONNECT_REFSTRUCT struct
-//#define SIMCONNECT_ENUM_FLAGS typedef DWORD
-#else // !MSVC
+#ifdef __clang__
+#define SIMCONNECT_REFSTRUCT struct [[clang::annotate("SIMCONNECT_REFSTRUCT")]]
+#define SIMCONNECT_ENUM_FLAGS [[clang::annotate("SIMCONNECT_ENUM_FLAGS")]] typedef DWORD
+#else // !__clang__
+#ifdef GNUC
 #define SIMCONNECT_REFSTRUCT struct __attribute__((annotate("SIMCONNECT_REFSTRUCT")))
-//#define SIMCONNECT_ENUM_FLAGS typedef __attribute__((flag_enum)) DWORD
-#endif // MSVC
-
-typedef DWORD SIMCONNECT_ENUM_FLAGS_VALUE;
-#define SIMCONNECT_ENUM_FLAGS typedef SIMCONNECT_ENUM_FLAGS_VALUE
+#define SIMCONNECT_ENUM_FLAGS __attribute__((annotate("SIMCONNECT_ENUM_FLAGS"))) typedef DWORD
+#else // !GNUC
+#define SIMCONNECT_REFSTRUCT struct
+#define SIMCONNECT_ENUM_FLAGS typedef DWORD
+#endif // !GNUC
+#endif // !__clang__
