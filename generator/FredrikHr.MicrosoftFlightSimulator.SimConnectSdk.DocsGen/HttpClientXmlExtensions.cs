51e62791be380c9847e19acaaf29207943a708c6
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2024-12-01 15:20:03 +0100

    Add GetXmlDocumentAsync method

--- generator/FredrikHr.MicrosoftFlightSimulator.SimConnectSdk.DocsGen/HttpClientXmlExtensions.cs
+++ generator/FredrikHr.MicrosoftFlightSimulator.SimConnectSdk.DocsGen/HttpClientXmlExtensions.cs
@@ -6,6 +6,41 @@ namespace FredrikHr.MicrosoftFlightSimulator.SimConnectSdk.DocsGen;
 
 public static class HttpClientXmlExtensions
 {
+    public static async Task<XmlDocument> GetXmlDocumentAsync(
+        this HttpClient httpClient,
+        Uri? requestUri,
+        XmlReaderSettings? xmlSettings,
+        CancellationToken cancelToken = default
+        )
+    {
+        ArgumentNullException.ThrowIfNull(httpClient);
+
+        HttpResponseMessage respMsg = await httpClient.GetAsync(
+            requestUri,
+            HttpCompletionOption.ResponseHeadersRead,
+            cancelToken
+            ).ConfigureAwait(continueOnCapturedContext: false);
+        Task<Stream> respStreamTask = respMsg.Content.ReadAsStreamAsync(cancelToken);
+        string? respCharset = respMsg.Content.Headers.ContentType?.CharSet;
+        Encoding? respEnc = null;
+        if (!string.IsNullOrEmpty(respCharset))
+        {
+            try
+            {
+                respEnc = Encoding.GetEncoding(respCharset);
+            }
+            catch (ArgumentException) { }
+        }
+        using Stream respStream = await respStreamTask.ConfigureAwait(continueOnCapturedContext: false);
+        using StreamReader respStreamReader = respEnc is null
+            ? new(respStream)
+            : new(respStream, respEnc);
+        using XmlReader respXmlReader = XmlReader.Create(respStreamReader, xmlSettings);
+        XmlDocument respXmlDoc = new();
+        respXmlDoc.Load(respXmlReader);
+        return respXmlDoc;
+    }
+
     public static async Task<XPathDocument> GetXPathDocumentAsync(
         this HttpClient httpClient,
         Uri? requestUri,
